5205626ac21064f817e0c65b5a04dd0c34e00a4c
Apiwat Lee <apiwatz.lee@gmail.com>
2025-04-02 13:31:12 +0700

    edit:ignore dist folder

--- dist/routes/user.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const user_controller_1 = require("../controllers/user.controller");
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get('/users', user_controller_1.getDepartmentSummary);
-exports.default = router;
